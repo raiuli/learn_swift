d1275c8a9ba1b093c18e6a12dd0e4275cd450ec1
Raihan Ul Islam <raihan.ul.islam@ericsson.com>
2022-08-26 07:12:37 +0000

    test comit

--- learn_clouser/Sources/learn_clouser/learn_clouser.swift
+++ learn_clouser/Sources/learn_clouser/learn_clouser.swift
@@ -4,5 +4,8 @@ public struct learn_clouser {
 
     public static func main() {
         print(learn_clouser().text)
+        let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"] 
+        let reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in return s1 > s2 } )
+        print(reversedNames)
     }
 }
