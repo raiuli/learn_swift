943b1b2de314093fc639b4b7538fc7b6728fbb16
Raihan Ul Islam <raihan.ul.islam@ericsson.com>
2022-08-26 21:27:10 +0000

    sdfs

--- learn_clouser/Sources/learn_clouser/learn_clouser.swift
+++ learn_clouser/Sources/learn_clouser/learn_clouser.swift
@@ -1,11 +1,47 @@
 @main
 public struct learn_clouser {
     public private(set) var text = "Hello, World!"
-
+    public indirect enum ArithExp{
+            case number(Int)
+            case addition(ArithExp,ArithExp)
+            case multiplication(ArithExp,ArithExp)
+        }
     public static func main() {
         print(learn_clouser().text)
         let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"] 
         let reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in return s1 > s2 } )
         print(reversedNames)
+        enum Beverages: CaseIterable{
+            case coffe, tea, juice
+        }
+         print("\(reversedNames) beverages available")
+        let numberOfCoices=Beverages.allCases.count
+        print("\(numberOfCoices) beverages available")
+        for beverage in Beverages.allCases{
+            print(beverage)
+        }
+        enum Barcode{
+            case upc(Int, Int, Int, Int)
+            case qrCode(String)
+        }
+        let productBarcode=Barcode.upc(8,8590, 51226,3)
+        print(productBarcode)
+        switch productBarcode{
+            case .upc(let ns,let man, let pr, let chk):
+                print("\(ns),\(man),\(pr),\(chk)")
+            case .qrCode(let pc):
+                print("\(pc)")
+        }
+        
+        let five = ArithExp.number(5)
+        let four = ArithExp.number(4)
+        let sum = ArithExp.addition(five, four)
+        print("\(sum)")
+        print(evaluate(sum))
+
     }
+    public static func evaluate(_ expression: ArithExp) -> Int {
+s
+    }
+}
 }
